e6b833603abe7487101945ad87826da4c8535a42
Eric Chau <eric.chau@lp-digital.fr>
2014-12-31 15:16:50 +0100

    [public] + added index.php which instanciate an BackBee\Standard\Application and start it

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+
+if (!is_file($autoloadFile = __DIR__.'/../vendor/autoload.php')) {
+    throw new \LogicException('Could not find autoload.php in vendor/. Did you run "composer install --dev"?');
+}
+
+require $autoloadFile;
+
+$context = null;
+$environment = null;
+
+$application = new \BackBee\Standard\Application($context, $environment);
+$application->start();
