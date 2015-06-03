7d06d418485fbbd481a2234d5311e2cb7171a66b
fkroockmann <florian.kroockmann@lp-digital.fr>
2015-06-03 15:33:18 +0200

    Fix navbar

--- repository/Templates/helpers/navbar.php
+++ repository/Templates/helpers/navbar.php
@@ -5,45 +5,25 @@ namespace BackBee\Renderer\Helper;
 class navbar extends AbstractHelper
 {
 
-    public function __invoke($position, $mode = null)
+    public function __invoke($position)
     {
         $application = $this->_renderer->getApplication();
         $repository = $application->getEntityManager()->getRepository('BackBee\NestedNode\Page');
 
         $sections = [];
-        $articles = [];
+        $currentPage = $this->_renderer->getCurrentPage();
         $selected = null;
 
-        if (null !== $current = $this->_renderer->getCurrentPage()) {
-            $selected = $repository->getAncestor($current, 1);
-            $sections = $repository->getVisibleDescendants($current->getRoot(), 2);
-
-            foreach ($sections as $page) {
-                if (1 === $page->getLevel()) {
-                    $selector = [
-                        'parentnode' => [$page->getUid()],
-                        'orderby'    => ['modified', 'desc'],
-                        'limit'      => 6,
-                    ];
-
-                    $articles[$page->getUid()] = $application->getEntityManager()
-                        ->getRepository('BackBee\ClassContent\AbstractClassContent')
-                        ->getSelection($selector, false, true, 0, 3, true, false, ['BackBee\ClassContent\Article'])
-                    ;
-                }
-            }
+        if (null !== $currentPage) {
+            $selected = $repository->getAncestor($currentPage, 1);
+            $sections = $repository->getVisibleDescendants($currentPage->getRoot(), 1);
         }
 
-        $is_header = false;
-        if ($position == 'header') {
-            $is_header = true;
-        }
-
-        $render = $this->_renderer->partial('partials/navbar.' . (null !== $mode ? $mode . '.' : '') . 'twig', [
+        $render = $this->_renderer->partial('partials/navbar.twig', [
             'sections' => $sections,
-            'articles' => $articles,
             'selected' => $selected,
-            'is_header' => $is_header,
+            'is_header' => $position === 'header',
+            'is_root' => ($currentPage !== null) ? $currentPage->isRoot() : false
         ]);
 
         return $render;
