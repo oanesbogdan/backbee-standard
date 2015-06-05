415a8f0336db4fb60f0ecebd72e97bdb7c930fb9
fkroockmann <florian.kroockmann@lp-digital.fr>
2015-06-05 14:48:00 +0200

    Update resources (css/js)

--- repository/Resources/js/script.js
+++ repository/Resources/js/script.js
@@ -1,11 +1,11 @@
 /*
-Lp digital - v1.2 June2015
+Lp digital - v1.2.1 June2015
 Backbee Demo
 required: jquery
 */
 
 
-$(document).ready(function() {
+jQuery(document).ready(function() {
 
   //init SHORTCUTS
   $("#access-shortcuts-wrapper>ul").initShortcuts();
@@ -37,86 +37,60 @@ $(document).ready(function() {
   $('.btn-printer').on('click', function(e) {
       e.preventDefault(); window.print();
   });
-
+  
   //Video RWD
-  $(".content-video iframe").resizeEmbed();
+  $(".content-video iframe").resizeEmbed(); 
+	
+  //RWD table - 991px
+  $("table.table-responsive").tableRwd({breakpoint:991});
 
-  //Smooth scroll & and positionning anchor under sticky header
-  $('a[href*=#]:not([href=#]):not([data-toggle])').click(function() {
-    var stickyHeight = 100;
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top-stickyHeight //50px header affix
-        }, 1000);
-      }
-    }
-  });
+  //NAV main - .rwd-is-desktop 
+  $('.navbar-wrapper').affix(); 
+
 
 //END ready
 });
 
+
 // Window Load
 $(window).load(function(){
-  //owl - company
-  var owlCompany = $('.owl-fw').owlCarousel({
-    pagination: true,
-    itemsCustom: [[0,1]],
-    navigation: false,
-    autoHeight: true,
-    navigation: true,
-    navigationText: ["<i class=\"fa fa-angle-left\"></i>","<i class=\"fa fa-angle-right\"></i>"]
-  });
-
-  //
-  $('.owl-demo').each(function(){
-    var owl = $(this);
-    var hasPagination = ($(this).hasClass("owl-pagination")) ? true : false;
-    owl.owlCarousel({
-      pagination: hasPagination,
-      navigation: true,
-      autoPlay: 6000, //default 5000
-      autoHeight: true,
-      itemsCustom: [[0, 2], [768, 4]],
-      navigationText: ["<i class=\"fa fa-angle-left\"></i>","<i class=\"fa fa-angle-right\"></i>"],
-      afterInit : function(){
-        owl.removeClass('slider-loader');
-      }
-    });
-  });
-  $('.owl-slider').each(function(){
-    var owl = $(this);
-    var hasPagination = ($(this).hasClass("owl-pagination")) ? true : false;
-    owl.owlCarousel({
-      pagination: hasPagination,
-      navigation: true,
-      autoPlay: 6000, //default 5000
-      autoHeight: true,
-      navigationText: ["<i class=\"fa fa-angle-left\"></i>","<i class=\"fa fa-angle-right\"></i>"],
-      afterInit : function(){
-        owl.removeClass('slider-loader');
-      }
-    });
-  });
-  //
+	//FW
+	$('.slider-wrapper.slider-fw .slider-list').each(function(){
+		var owl = $(this);
+		var hasPagination = ($(this).hasClass("slider-has-pagination")) ? true : false;
+		owl.owlCarousel({
+			pagination: hasPagination, 
+			singleItem: true, 
+			navigation: true, 
+			autoPlay: 6000, //default 5000 
+			autoHeight: true, 
+			lazyLoad: true, 
+			navigationText: ["<i class=\"fa fa-angle-left\"></i>","<i class=\"fa fa-angle-right\"></i>"],
+			afterInit : function(){
+				//console.log("ignition done");
+				//
+				owl.parent(".slider-wrapper").removeClass('slider-loader');
+				//
+			}, 
+			beforeMove : function(){
+				//console.log("move done");
+				//
+				owl.parent(".slider-wrapper").find('.owl-item').eq(this.owl.currentItem).find('.figcaption .ttl').removeClass('animated fadeIn');
+				//
+			},
+			afterMove : function(){
+				//console.log("move done");
+				//
+				owl.parent(".slider-wrapper").find('.owl-item').eq(this.owl.currentItem).find('.figcaption .ttl').addClass('animated fadeIn');
+				//
+			}
+		//
+		});
+	});
+	//
 
 });
 // End window load
-
-$(".follow-link").on("click",function(e){
-  var _self = $(this);
-  var follow = _self.find(_self.data("follow"));
-  if(!follow.is(e.target)){
-    window.location.href = follow.attr("href");
-  }
-});
-
-$(".follow-link .btn-info").on("click",function(e){
-  e.preventDefault;
-  console.log("click",e);
-});
 
 
 //initShortcuts
@@ -124,9 +98,9 @@ $.fn.initShortcuts = function(options) {
 
   var obj = $(this);
   obj.find('a').focus(function(e) {obj.css('height', 'auto'); });
-  obj.find('a').blur(function(e) {obj.css('height', '0px'); });
-
-  return this;
+  obj.find('a').blur(function(e) {obj.css('height', '0px'); });     
+  
+  return this;        
 };
 
 
@@ -148,14 +122,57 @@ $.fn.resizeEmbed = function(options) {
       .height(newWidth * $(this).attr('data-aspectRatio'));
     });
 
-    $(window).on("resize",function() {
+    $(window).on("resize",function() { 
        obj.each(function() {
         var newWidth = $(this).parent().width();
         $(this)
           .width(newWidth)
           .height(newWidth * $(this).attr('data-aspectRatio'));
-       });
+       });                                
     });
 
     return this;
+};
+
+
+//FN Table RWD
+//MOD - based on: http://zurb.com/playground/playground/responsive-tables/responsive-tables.js 
+$.fn.tableRwd = function(options) {
+  var defaults = {
+    breakpoint : 991
+  };
+  var options = $.extend(defaults, options);
+  var obj = $(this);
+
+  var switched = false;
+  var updateTables = function () {
+    if (($(window).width() < options.breakpoint) && !switched) {
+      switched = true;
+      obj.each(function (i, element) {
+        splitTable($(element));
+      });
+      return true;
+    } else if (switched && ($(window).width() > options.breakpoint)) {
+      switched = false;
+      obj.each(function (i, element) {
+        unsplitTable($(element));
+      });
+    }
+  };
+
+  $(window).load(updateTables);
+  $(window).bind("resize", updateTables);
+
+  function splitTable(original) {
+    original.attr("data-margin",original.css("margin")).css("margin",0).wrap(
+      $("<div class='table-wrapper'></div>").css({"overflow-x":"scroll", "overflow-y":"visible", "margin": original.attr("data-margin")})
+    );
+  }
+
+  function unsplitTable(original) {
+    original.css("margin","").removeAttr("data-margin");
+    original.unwrap();
+  }
+
+  return this;
 };
