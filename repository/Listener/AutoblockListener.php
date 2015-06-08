30b0e829c84787bf060122168695172a6327abaf
fkroockmann <florian.kroockmann@lp-digital.fr>
2015-06-08 11:56:25 +0200

    Add autoblock

--- repository/Listener/AutoblockListener.php
+++ repository/Listener/AutoblockListener.php
@@ -1,55 +1,81 @@
 <?php
+
+/*
+ * Copyright (c) 2011-2015 Lp digital system
+ *
+ * This file is part of BackBee Standard Edition.
+ *
+ * BackBee is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BackBee Standard Edition is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with BackBee Standard Edition. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 namespace BackBee\Event\Listener;
 
 use BackBee\Event\Event;
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+use Doctrine\ORM\Tools\Pagination\Paginator;
 
+/**
+ * Autoblock Listener
+ *
+ * @author      f.kroockmann <florian.kroockmann@lp-digital.fr>
+ */
 class AutoblockListener extends Event
 {
+    private static $application;
+    private static $em;
+
     public static function onRender(Event $event)
     {
-        $dispatcher = $event->getDispatcher();
         $renderer = $event->getEventArgs();
+        self::$application = $renderer->getApplication();
+        self::$em = self::$application->getEntityManager();
+
         $content = $renderer->getObject();
-        $application = $renderer->getApplication();
 
-        $renderer->assign('contents', []);
-        if (NULL != $selector = $content->getParam('selector')) {
-            $selector = reset($selector);
+        $selector = ['parentnode' => self::getParentNode($content->getParamValue('parent_node'))];
 
-            if (!array_key_exists('parentnode', $selector) || 0 == count($selector['parentnode'])) {
-                if (NULL !== $renderer->getCurrentPage()) {
-                    $selector['parentnode'] = [$renderer->getCurrentPage()->getUid()];
-                } else {
-                    $selector['parentnode'] = null;
-                }
+        $contents = self::$em->getRepository('BackBee\ClassContent\AbstractClassContent')
+                             ->getSelection(
+                                 $selector,
+                                 in_array('multipage', $content->getParamValue('multipage')),
+                                 in_array('recursive', $content->getParamValue('recursive')),
+                                 (int) $content->getParamValue('start'),
+                                 (int) $content->getParamValue('limit'),
+                                 true,
+                                 false,
+                                 (array) $content->getParamValue('content_to_show'),
+                                 (int) $content->getParamValue('delta')
+                             );
+
+        $count = $contents instanceof Paginator ? $contents->count() : count($contents);
+
+        $renderer->assign('contents', $contents);
+        $renderer->assign('nbContents', $count);
+    }
+
+    private static function getParentNode($parentNodeParam)
+    {
+        $parentNode = null;
+
+        if (!empty($parentNodeParam)) {
+            if (isset($parentNodeParam['pageUid'])) {
+                $parentNode = self::$em->getRepository('BackBee\NestedNode\Page')->find($parentNodeParam['pageUid']);
             }
-
-            $limit = array_key_exists('limit', $selector) ? $selector['limit'] : 10;
-
-            if (array_key_exists('classcontent', $selector)) {
-                $classcontents = (array)$selector['classcontent'];
-
-                $contentsList = $application->getEntityManager()
-                                            ->getRepository('BackBee\ClassContent\AbstractClassContent')
-                                            ->getSelection(
-                                                $selector,
-                                                false,
-                                                false,
-                                                0,
-                                                $limit,
-                                                true,
-                                                false,
-                                                $classcontents
-                                            );
-            }
+        } else {
+            $parentNode = self::$application->getCurrentPage();
         }
 
-        $renderer->assign('contents', $contentsList);
-        $renderer->assign('nbContent', count($contentsList));
+        return $parentNode;
     }
 }
